77dad41908bee4cfec05853d705be191745e60fa
prosper edafe <mamusedafe19@gmail.com>
2024-10-19 00:52:03 +0100

    "Update initial route name in _layout.tsx, remove SplashScreen.tsx, and add a child component to AuthPagesWrapper.tsx"

--- components/SplashScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native"
-
-const SplashScreen = () => {
-    return (
-        <View>xvnikge</View>
-    )
-}
-
-export default SplashScreen;
--- components/containers/AuthPagesWrapper.tsx
+++ components/containers/AuthPagesWrapper.tsx
@@ -16,6 +16,7 @@ export const AuthPagesWrapper = ({ children }: IAuthPagesWrapper) => {
                 <Svg style={styles.pattern} width="77" height="72" viewBox="0 0 77 72" fill="none">
                     <Circle cx="90.5" cy="-18.5" r="90.5" fill="#FE724C" />
                 </Svg>
+
                 {children}
             </ScrollView>
         </KeyboardAvoidingView>
